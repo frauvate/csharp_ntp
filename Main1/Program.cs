86026f6aa0270916a10178ec445637ae6c839309
esma <146848430+frauvate@users.noreply.github.com>
2024-10-16 19:52:10 +0300

    Add files via upload

---
+++ Main1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Spiral Matris: NxN boyutlarında oluşturulan bir matrisin ve bu matrisi spiral şekilde yazdıracak program
+namespace Main1
+{
+    class SpiralMatrix
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Matris boyutunu girin (N): "); //kullanıcıdan spiralin sonu için bir değer alınması
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n]; //alınan n değeri matrixin boyutlarını oluşturur
+
+            int value = 1; // Başlangıç değeri. yazdırma işlemibu değerden başlar
+            int minRow = 0, maxRow = n - 1;
+            int minCol = 0, maxCol = n - 1;
+
+            // Spiral matris doldurma işlemi
+            while (value <= n * n)
+            {
+                // Yukarıdan sağa doğru
+                for (int i = minCol; i <= maxCol; i++)
+                    matrix[minRow, i] = value++;
+                minRow++;
+
+                // Sağdan aşağıya doğru
+                for (int i = minRow; i <= maxRow; i++)
+                    matrix[i, maxCol] = value++;
+                maxCol--;
+
+                // Aşağıdan sola doğru
+                for (int i = maxCol; i >= minCol; i--)
+                    matrix[maxRow, i] = value++;
+                maxRow--;
+
+                // Soldan yukarıya doğru
+                for (int i = maxRow; i >= minRow; i--)
+                    matrix[i, minCol] = value++;
+                minCol++;
+            }
+
+            // Spiral matrisin yazdırılması
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ Main3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//N'e Kadar Asal Sayıların Toplamı: Kullanıcıdan alınan N sayısına kadar olan tüm asal sayıların toplamını bulan bir program
+namespace Main3
+{
+    class PrimeSum
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N sayısını girin: "); //son sayı olarak belirlenecek sayı kullanıcıdan alınır
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                if (IsPrime(i)) // isprime metodunu çağırıp, asalsa toplama ekler, değilse sonraki sayıya geçer
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"N'e kadar olan asal sayıların toplamı: {sum}");
+            Console.Read();
+        }
+
+        // Asal sayı kontrol fonksiyonu
+        static bool IsPrime(int number)
+        {
+            if (number <= 1) //1'den küçük sayılar asal değildir
+                return false;
+            for (int i = 2; i <= Math.Sqrt(number); i++) //sayının karekökünü tam bölen 1 ve kendisi hariç bir sayı varsa, asal değildir
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true; //kalan durumlarda sayı asaldır
+        }
+    }
+}
